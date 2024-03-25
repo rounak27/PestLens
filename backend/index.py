be84b566c2507d4491a5750c9f3b36a48dfd8555
Rounak Rajbhandari <rounak.rajbhandari@gmail.com>
2024-03-25 18:31:28 +0545

    Comitted

--- backend/index.py
+++ backend/index.py
@@ -1,21 +1,26 @@
 import cv2
 import numpy as np
-from keras.preprocessing import image
-from keras.applications.mobilenet import preprocess_input
-from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras.models import load_model
 import json
 import sys
+import os
 
 # Load the trained model
-model = load_model('pestdetectmodelbest.h5') 
+try:
+    model = load_model('pestdetectmodel.h5')
+except Exception as e:
+    print(f"Error loading model: {e}")
+    sys.exit(1)
 
 # Define class labels
-class_labels = ['aphids', 'armyworm', 'bettel', 'boolworm','fruit_flies', 'grasshopper','hornworms', 'mites', 'mosquito', 'sawfly', 'stem_borer']
+class_labels = ['aphids', 'armyworm', 'bettel', 'boolworm', 'fruit_flies', 'grasshopper', 'hornworms', 'mites', 'mosquito', 'sawfly', 'stem_borer']
 
 # Function to check if predicted label exists in class labels
 def check_label_exists(prediction, class_labels):
     if int(np.argmax(prediction)) not in range(len(class_labels)):
-        return f"Error: Predicted label '{class_labels[int(np.argmax(prediction))]}'' does not exist."
+        return f"Error: Predicted label '{class_labels[int(np.argmax(prediction))]}' does not exist."
     else:
         return None
 
@@ -24,18 +29,30 @@ if len(sys.argv) != 2:
     print("Usage: python index.py <image_path>")
     sys.exit(1)
 
+# Get the image path from the command-line argument
+image_path = sys.argv[1]
+
+# Check if the image file exists
+if not os.path.isfile(image_path):
+    print(f"Error: Image file not found at '{image_path}'.")
+    sys.exit(1)
+
 # Load and preprocess the new image
-img_path = sys.argv[1]
-img = image.load_img(img_path, target_size=(224, 224))
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)
-img_array = preprocess_input(img_array)
+try:
+    img = image.load_img(image_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+except Exception as e:
+    print(f"Error loading image: {e}")
+    sys.exit(1)
 
 # Make the prediction
 predictions = model.predict(img_array)
 
 # Set confidence threshold for insect classification
 confidence_threshold = 0.5
+
 # Check if any insect class prediction exceeds confidence threshold
 if np.max(predictions) < confidence_threshold:
     result = {"predicted_class": "No insect class predicted.", "confidence": 0.0}
